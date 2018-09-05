1108aa8aaa1ef26cfb30f6bc9d0a522a1e67151d
LEANDRO ESTEBAN GALLIPPI <leandro.gallippi@mercadolibre.com>
2018-09-04 23:15:16 -0300

    En la pantalla Home muestro un ejemplo de utilización del componente table

--- web-app/src/components/Home.js
+++ web-app/src/components/Home.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Layout from './Layout';
+import Table from './Table';
+import { requestData } from '../utils/mockData';
+
+const columns = [
+  {
+    Header: "First Name",
+    accessor: "firstName"
+  },
+  {
+    Header: "Last Name",
+    id: "lastName",
+    accessor: d => d.lastName
+  },
+  {
+    Header: "Age",
+    accessor: "age"
+  }
+];
 
 const Home = () => {
   return (
     <Layout title="Home">
       <p>Hello World of React and Webpack! Hot reloaded</p>
+      <Table requestData={requestData} columns={columns} />
     </Layout>
   );
 };
