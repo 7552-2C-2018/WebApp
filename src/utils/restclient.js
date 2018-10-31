b0864a106d73bd0e148756bcd1476fe6225f8837
LEANDRO ESTEBAN GALLIPPI <leandro.gallippi@mercadolibre.com>
2018-10-31 14:19:08 -0300

    Refactor

--- src/utils/restclient.js
+++ src/utils/restclient.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import store from 'store';
 
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwiaWF0IjoxNTM5MDQ3OTg2LCJleHAiOjE1Mzk2NTI3ODZ9.XtIDvQQ1MlnC6wGy54cNmp0OnXgyIInMQjUAHARAsyk';
-
 const restclient = () => (
   axios.create({
     baseURL: 'http://shared-server-25.herokuapp.com',
